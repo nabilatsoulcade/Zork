f4147cfad64af0c575b98b4d7c781bc57176ca67
Nabil Sekirime <nabil@soulcadeinteractive.com>
2019-09-12 18:34:50 -0400

    Looking Around and Quitting

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -7,6 +7,27 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            //Ask for input and normalizes it.
+            string inputString = Console.ReadLine();
+            inputString = inputString.ToUpper();
+
+            //Interpets Input
+
+            if (inputString == "QUIT")
+            {
+                Console.WriteLine("Thank you for playing");
+            }
+
+            else if (inputString == "LOOK")
+            {
+                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+            }
+
+            else
+            {
+                Console.WriteLine("Unrecognized command.");
+            }
         }
     }
 }
