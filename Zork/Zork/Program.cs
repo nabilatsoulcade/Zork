c7714f5c4c3d54fb4ed736d5e0ab92bca239880e
Nabil Sekirime <nabil@soulcadeinteractive.com>
2019-10-01 18:01:53 -0400

    The Game Loop

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -18,10 +18,37 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            //Ask for input and normalizes it.
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door";
+                        break;
+
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        outputString = $"You moved {command}";
+                        break;
+
+                    default:
+                        outputString = "Unknown command";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
         }
 
         private static Commands ToCommand(string commandString)
