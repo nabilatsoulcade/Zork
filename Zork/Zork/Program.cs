02c8d463f745ef7e243f2ed0d2158086ffd47c6d
Nabil Sekirime <nabil@soulcadeinteractive.com>
2019-10-04 16:04:25 -0400

    Expanding the Foundation

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -26,10 +26,10 @@ namespace Zork
         }
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Zork!");
             InializeRoomDescriptions();
 
-            Console.WriteLine("Welcome to Zork!");
-
+            Room previousRoom = null;
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
@@ -114,17 +114,22 @@ namespace Zork
         };
         private static void InializeRoomDescriptions()
         {
-            Rooms[0, 0].Description = "You are on a rock-stewn trail.";                                                                             // Rocky Trail
-            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";     // South of House
-            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";                                                  // Canyon View
+            var roomMap = new Dictionary<string, Room>();
+            foreach(Room room in Rooms)
+            {
+                roomMap[room.Name] = room;
+            }
+            roomMap["Rocky Trail"].Description = "You are on a rock-stewn trail.";                                                                             // Rocky Trail
+            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";     // South of House
+            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";                                                  // Canyon View
 
-            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";                                                 // Forest
-            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";                                    // West of House
-            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar."; // Behind House
-            
-            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";        // Dense Woods
-            Rooms[2, 1].Description = "You are facing north side of a white house. There is no door here, and all the windows are barred.";         // North of House
-            Rooms[2, 2].Description = "You are in a clearing, with a forest surronding you on the west and south.";                                 // Clearing
+            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";                                                 // Forest
+            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";                                    // West of House
+            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar."; // Behind House
+
+            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";        // Dense Woods
+            roomMap["North of House"].Description = "You are facing north side of a white house. There is no door here, and all the windows are barred.";         // North of House
+            roomMap["Clearing"].Description = "You are in a clearing, with a forest surronding you on the west and south.";                                 // Clearing
         }
         private static readonly List<Commands> Directions = new List<Commands>
         {
